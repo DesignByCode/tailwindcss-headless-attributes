0f233519dd640147d74fedcfa047bd24bad6246f
claudemyburgh <claudemyburgh@gmail.com>
2022-11-06 21:34:17 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-"# tailwindcss-headless-attributes" 
+# TailwindCSS Headless Attributes
+A plugin that provides utilities for adding styles with aria attributes. Create to play nice with [vanilla-headless](https://www.npmjs.com/package/vanilla-headless)
+## Installation
+
+
+Install the plugin from npm:
+```bash
+npm install tailwindcss-headless-attributes
+```
+Then add the plugin to your tailwind.config.js file:
+```javascript
+// tailwind.config.js
+module.exports = {
+  theme: {
+    
+  },
+  plugins: [
+    require("tailwindcss-headless-attributes"),
+  ],
+}
+```
+
+## Usage
+Use ``aria-*`` and ``data-*`` attributes to style html elements
+```html
+<headless-toggle class="flex items-center group aria-disabled:bg-gray-300" aria-disabled="true">
+    <button aria-checked="true" class="group-data-switch-on:bg-blue-500 bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2  border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="button">
+        <span class="sr-only">Use setting</span>
+        <span aria-hidden="true" class="group-data-switch-on:translate-x-5 translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-50" data-switch></span>
+    </button>
+</headless-toggle>
+```
+
+### Data Attributes for Vanilla-Headless
+Use these vanilla-headless attributes by using``data-{name}-{value}``
+
+```html
+....
+<button class="data-switch-on:bg-indigo-500">
+    <span class="data-switch-off:translate-x-0 data-switch-on:translate-x-5"></span>
+</button>
+...
+```
+| Attribute          | Variant          |
+|:-------------------|:-----------------|
+| data-state="open"  | data-state-open  |
+| data-state="close" | data-state-close |
+| data-switch="on"   | data-switch-on   |
+| data-switch="off"  | data-switch-off  |
+
+
+
+### Aria Attributes 
+These ``aria-*`` attribute all translate to a value of ``true``
+```css
+[aria-pressed="true"].aria-pressed\:shadow-sm {
+    --tw-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+    --tw-shadow-colored: 0 1px 2px 0 var(--tw-shadow-color);
+    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+}
+```
+- aria-atomic
+- aria-busy
+- aria-checked
+- aria-current
+- aria-disabled
+- aria-expanded
+- aria-grabbed
+- aria-haspopup
+- aria-hidden
+- aria-invalid
+- aria-live
+- aria-modal
+- aria-multiline
+- aria-multiselectable
+- aria-pressed
+- aria-readonly
+- aria-required
+- aria-selected
---
+++ index.js
@@ -0,0 +1,86 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(({addVariant, theme}) => {
+  /**
+   * https://tailwindcss.com/docs/plugins#parent-and-sibling-states
+   * @param name
+   * @param value
+   * @param prefix
+   * @param booleanGenerator
+   */
+  const variantGenerator = (name, value, prefix = null, booleanGenerator = true) => {
+    if (booleanGenerator) {
+      addVariant(`${prefix}-${name}`, `[${prefix}-${name}="${value}"]& `)
+      addVariant(`peer-${prefix}-${name}`, `:merge(.peer)[${prefix}-${name}="${value}"] ~ &`)
+      addVariant(`group-${prefix}-${name}`, `:merge(.group)[${prefix}-${name}="${value}"] &`)
+    } else {
+      addVariant(`${prefix}-${name}-${value}`, `[${prefix}-${name}="${value}"]&`)
+      addVariant(`peer-${prefix}-${name}-${value}`, `:merge(.peer)[${prefix}-${name}="${value}"] ~ &`)
+      addVariant(`group-${prefix}-${name}-${value}`, `:merge(.group)[${prefix}-${name}="${value}"] &`)
+    }
+  }
+
+  /**
+   * aria attribute with true and false value
+   * @type {string[]}
+   */
+  const booleanAttributes = [
+    "atomic",
+    "busy",
+    "checked",
+    "current",
+    "disabled",
+    "expanded",
+    "grabbed",
+    "haspopup",
+    "hidden",
+    "invalid",
+    "live",
+    "modal",
+    "multiline",
+    "multiselectable",
+    "pressed",
+    "readonly",
+    "required",
+    "selected",
+  ]
+
+  /**
+   * custom data attribute for vanilla-headless
+   * https://www.npmjs.com/package/vanilla-headless
+   * @type {{state: string[], switch: string[]}}
+   */
+  const headlessDataSets = {
+    state: ['open', 'close'],
+    switch: ['on', 'off'],
+  }
+
+  /**
+   * aria attribute with list type
+   * @type {{haspopup: string[], relevant: string[], current: string[], orientation: string[], autocomplete: string[], pressed: string[], invalid: string[], sort: string[], activedescendant: string[], dropeffect: string[], live: string[]}}
+   */
+  const ariaAttributesList = {
+    activedescendant: ["application", "combobox", "composite","group", "textbox"],
+    autocomplete: [ "none", "both", "inline", "list" ],
+    current: [ "page", "step", "location", "date", "time" ],
+    dropeffect: [ "copy", "execute", "link", "move", "none", "popup" ],
+    haspopup: [ "menu", "listbox", "tree", "grid", "dialog"],
+    invalid: ["grammar", "spelling"],
+    live: ["assertive", "off", "polite"],
+    orientation: [ "horizontal", "undefined", "vertical" ],
+    pressed: ["mixed"],
+    relevant: [ "additions", "all", "removals", "text" ],
+    sort: [ "ascending", "descending", "none", "other" ],
+  }
+
+  booleanAttributes.forEach(attr => variantGenerator(attr, "true", "aria"))
+
+  for (const [attr, values] of Object.entries(headlessDataSets)) {
+    values.forEach(value => variantGenerator(attr , value, 'data', false))
+  }
+
+  for (const [attr, values] of Object.entries(ariaAttributesList)) {
+    values.forEach(value => variantGenerator(attr , value, 'aria', false))
+  }
+
+})
