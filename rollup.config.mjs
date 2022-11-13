dece74e3fd3976a90871f300b4f2f3038b34e215
claudemyburgh <claudemyburgh@gmail.com>
2022-11-13 03:54:27 +0200

    wip

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import pkg from "./package.json" assert {type: "json"}
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 
 
 export default [
